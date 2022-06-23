8fea55a8a24c2f57a2ae096e3e7ee10419b157fb
Neelam <neelam0101singh@gmail.com>
2022-06-23 21:37:34 +0530

    css  added

--- src/Components/Filters.jsx
+++ src/Components/Filters.jsx
@@ -5,21 +5,25 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import RadioGroup from "@mui/material/RadioGroup";
+import { fontSize } from "@mui/system";
 
 const Filters = ({brandsBar, handleFilter}) => {
    return (
       
-         <div className="sidebar" style={{ flexBasis: "16rem" }}>
+         <div className="sidebar" style={{ flexBasis: "16rem", marginRight:"4%" }}>
             <List
                sx={{
                   width: "100%",
                   maxWidth: 460,
                   bgcolor: "background.paper",
-                  padding: "10%"
+                  padding: "10%",
+                  color:"gray",
+                
                }}
             >
                {/* <Divider /> */}
-               <h2><b>Brands</b></h2>
+               <h2 style={{padding:"5%" ,background:"#e4e4e4" ,fontWeight:"700",color:"black",
+               boxShadow: "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;"}}>BRANDS</h2>
 
                {brandsBar.map((value, i) => {
                   const labelId = `checkbox-list-label-${value}`;
